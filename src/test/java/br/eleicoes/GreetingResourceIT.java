225c164e16f1a6d736ddcd8eb7b85e65a4abf3c3
etn-43 <elivelton.developer@gmail.com>
2023-09-05 15:17:46 -0300

    Projeto Quarkus criado

---
+++ src/test/java/br/eleicoes/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package br.eleicoes;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
